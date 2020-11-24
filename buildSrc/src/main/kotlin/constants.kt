94d4a8ee23a23ba7e37373fe19b307d3e04f782d
Drew Carlson <awcarlson@protonmail.com>
2020-11-23 19:23:32 -0800

    WIP: Add swift package

--- buildSrc/src/main/kotlin/constants.kt
+++ buildSrc/src/main/kotlin/constants.kt
@@ -1,3 +1,4 @@
 const val KOTLIN_VERSION = "1.4.20"
 const val KTOR_VERSION = "1.4.2"
 const val COROUTINES_VERSION = "1.4.1-native-mt"
+const val SWIFT_PACKAGE = "2.0.0"
--- swift-demo/build.gradle.kts
+++ swift-demo/build.gradle.kts
@@ -1,12 +1,31 @@
+import com.chromaticnoise.multiplatformswiftpackage.SwiftPackageExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
+    id("com.chromaticnoise.multiplatform-swiftpackage") version SWIFT_PACKAGE
+}
+
+configure<SwiftPackageExtension> {
+    swiftToolsVersion("5.3")
+    targetPlatforms {
+        watchOS { v("6") }
+        iOS { v("10") }
+        tvOS { v("9") }
+        macOS { v("10.0") }
+    }
 }
 
 kotlin {
+    val targets = mutableListOf<KotlinNativeTarget>()
+    ios { targets.add(this) }
+    tvos { targets.add(this) }
+    watchos { targets.add(this) }
+    macosX64("macos") { targets.add(this) }
 
-    iosX64 {
-        binaries {
-            framework(listOf(DEBUG)) {
+    targets.forEach { target ->
+        target.binaries {
+            framework(listOf(DEBUG, RELEASE)) {
                 baseName = "coingecko"
                 export(rootProject)
             }
@@ -14,10 +33,25 @@ kotlin {
     }
 
     sourceSets {
-        val iosX64Main by getting {
+        all { dependencies { api(rootProject) } }
+        val iosMain by getting {
             dependencies {
-                api(rootProject)
                 implementation("io.ktor:ktor-client-ios:$KTOR_VERSION")
+            }
+        }
+        val tvosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$KTOR_VERSION")
+            }
+        }
+        val watchosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$KTOR_VERSION")
+            }
+        }
+        val macosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$KTOR_VERSION")
             }
         }
     }
